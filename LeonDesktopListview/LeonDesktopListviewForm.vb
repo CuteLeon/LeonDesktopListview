4f793be80e7b8579ab14e462bfd174cd8543718f
Leon <xiaoyankeji@hotmail.com>
2016-08-26 20:20:50 +0800

    第一次提交

---
+++ LeonDesktopListview/LeonDesktopListviewForm.vb
@@ -0,0 +1,46 @@
+﻿Public Class LeonDesktopListviewForm
+    Private Declare Function GetWindowLong Lib "user32" Alias "GetWindowLongA" (ByVal hWnd As IntPtr, ByVal nIndex As Integer) As Integer
+    Private Declare Function SetWindowLong Lib "user32" Alias "SetWindowLongA" (ByVal hWnd As IntPtr, ByVal nIndex As Integer, ByVal dwNewLong As Integer) As Integer
+    Private Declare Function FindWindowEx Lib "user32" Alias "FindWindowExA" (ByVal hWnd1 As Integer, ByVal hWnd2 As Integer, ByVal lpsz1 As String, ByVal lpsz2 As String) As Integer
+    Private Declare Function GetDesktopWindow Lib "user32" Alias "GetDesktopWindow" () As IntPtr
+    Private Const GWL_STYLE = (-16) '获取窗口的窗口样式
+
+    Private Const LVS_SMALLICON = &H2 '小图标样式
+
+    Public Function GetDesktopIconHandle() As IntPtr
+        '获取物理系统桌面图标的句柄，用于嵌入实现置后显示
+        Dim HandleDesktop As Integer = GetDesktopWindow
+        Dim HandleTop As Integer = 0
+        Dim LastHandleTop As Integer = 0
+        Dim HandleSHELLDLL_DefView As Integer = 0
+        Dim HandleSysListView32 As Integer = 0
+        '在WorkerW结构里搜索
+        Do Until HandleSysListView32 > 0
+            HandleTop = FindWindowEx(HandleDesktop, LastHandleTop, "WorkerW", vbNullString)
+            HandleSHELLDLL_DefView = FindWindowEx(HandleTop, 0, "SHELLDLL_DefView", vbNullString)
+            If HandleSHELLDLL_DefView > 0 Then HandleSysListView32 = FindWindowEx(HandleSHELLDLL_DefView, 0, "SysListView32", "FolderView")
+            LastHandleTop = HandleTop
+            If LastHandleTop = 0 Then Exit Do
+        Loop
+        '如果找到了，立即返回
+        If HandleSysListView32 > 0 Then Return HandleSysListView32
+        '未找到，则在Progman里搜索(用于兼容WinXP系统)
+        Do Until HandleSysListView32 > 0
+            HandleTop = FindWindowEx(HandleDesktop, LastHandleTop, "Progman", "Program Manager")
+            HandleSHELLDLL_DefView = FindWindowEx(HandleTop, 0, "SHELLDLL_DefView", vbNullString)
+            If HandleSHELLDLL_DefView > 0 Then HandleSysListView32 = FindWindowEx(HandleSHELLDLL_DefView, 0, "SysListView32", "FolderView")
+            LastHandleTop = HandleTop
+            If LastHandleTop = 0 Then Exit Do : Return 0
+        Loop
+        Return HandleSysListView32
+    End Function
+
+    Private Sub LeonDesktopListviewForm_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Dim RegStartUp As Microsoft.Win32.RegistryKey = My.Computer.Registry.CurrentUser.CreateSubKey("Software\Microsoft\Windows\CurrentVersion\Run")
+        RegStartUp.SetValue("LeonDesktopListview", Application.ExecutablePath)
+
+        Dim DesktopHandle As IntPtr = GetDesktopIconHandle()
+        SetWindowLong(DesktopHandle, GWL_STYLE, GetWindowLong(DesktopHandle, GWL_STYLE) + LVS_SMALLICON)
+        End
+    End Sub
+End Class
